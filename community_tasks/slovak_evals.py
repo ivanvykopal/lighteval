3842995174e42e016d797adce414b9036d11ce3a
Ivan Vykopal <ivan.vykopal@gmail.com>
2025-11-21 11:28:40 +0000

    feat: add slovak custom tasks

--- community_tasks/slovak_evals.py
+++ community_tasks/slovak_evals.py
@@ -35,20 +35,28 @@ from lighteval.metrics.dynamic_metrics import (
     LogLikelihoodAccMetric,
 )
 from lighteval.metrics.metrics import Metrics
+from lighteval.metrics.dynamic_metrics import loglikelihood_acc_metric, multilingual_quasi_exact_match_metric, multilingual_quasi_f1_score_metric
 from lighteval.metrics.normalizations import LogProbCharNorm, LogProbPMINorm, LogProbTokenNorm
 from lighteval.tasks.multilingual.tasks import MMLU_SUBSETS
 from lighteval.tasks.multilingual.utils.task_utils import get_metrics_for_formulation
 from lighteval.tasks.templates.hellaswag import get_hellaswag_prompt_function
-from lighteval.tasks.templates.multichoice import get_mcq_prompt_function
+from lighteval.tasks.templates.multichoice import MULTI_CHOICE_QA_QUERY, get_mcq_prompt_function
 
 from lighteval.tasks.templates.nli import get_nli_prompt_function
-from lighteval.tasks.templates.utils.formulation import CFFormulation, HybridFormulation, MCFFormulation
+from lighteval.tasks.templates.qa import get_qa_prompt_function
+from lighteval.tasks.templates.utils.formatting_utils import capitalize, fix_ending_punct
+from lighteval.tasks.templates.utils.formulation import CFFormulation, HybridFormulation, MCFFormulation, build_choices
+from lighteval.tasks.templates.utils.translation_literals import TRANSLATION_LITERALS
 from lighteval.utils.language import Language
 from lighteval.tasks.default_prompts import LETTER_INDICES
 import lighteval.tasks.default_prompts as prompt
 from lighteval.tasks.lighteval_task import LightevalTaskConfig
 from langcodes import standardize_tag
+from langcodes import Language as LangCodeLanguage
 from lighteval.tasks.requests import Doc
+from lighteval.utils.language import Language, iso_639_3_ind_to_iso_639_3_macro, manage_duplicate_language_codes
+from lighteval.utils.utils import as_list
+
 
 
 # Hellaswag from ligtheval/mulitlingual
@@ -274,6 +282,545 @@ nli_lighteval_slovak_prompt = LightevalTaskConfig(
     trust_dataset=True,
     version=0,
 )
+
+belebele_tasks_slovak = [
+    LightevalTaskConfig(
+        name=f"community_belebele_{language}_{formulation.name.lower()}",
+        prompt_function=get_mcq_prompt_function(
+            iso_639_3_ind_to_iso_639_3_macro[LangCodeLanguage.get(language).to_alpha3()],
+            lambda line: {
+                "question": line["question"],
+                "context": line["flores_passage"],
+                "choices": [line[f"mc_answer{i}"] for i in range(1, 5)],
+                "gold_idx": int(line["correct_answer_num"]) - 1,
+            },
+            formulation=formulation,
+        ),
+        suite=("community",),
+        hf_repo="facebook/belebele",
+        hf_subset=language,
+        evaluation_splits=("test",),
+        hf_avail_splits=["test"],
+        few_shots_select=None,
+        metric=get_metrics_for_formulation(
+            formulation,
+            [
+                loglikelihood_acc_metric(normalization=LogProbTokenNorm()),
+                loglikelihood_acc_metric(normalization=LogProbCharNorm()),
+            ],
+        ),
+    )
+    for formulation in [MCFFormulation(), CFFormulation(), HybridFormulation()]
+    for language in [
+        "slk_Latn"
+    ]
+]
+
+sksquad_tasks = [
+    LightevalTaskConfig(
+        name=f"community_skquad_{language.value}",
+        prompt_function=get_qa_prompt_function(
+            language,
+            lambda line: {
+                "question": line["question"],
+                "context": line["context"],
+                "choices": [ans for ans in line["answers"]["text"] if len(ans) > 0],
+            },
+        ),
+        suite=("community",),
+        hf_repo="TUKE-DeutscheTelekom/skquad",
+        hf_subset="skquad",
+        evaluation_splits=("validation",),
+        hf_filter=lambda line: any(len(ans) > 0 for ans in line["answers"]["text"]),
+        few_shots_split="train",
+        generation_size=400,
+        stop_sequence=("\n",),
+        metric=(
+            multilingual_quasi_exact_match_metric(language, "prefix"),
+            multilingual_quasi_f1_score_metric(language),
+        ),
+    )
+    for language in [
+        Language.SLOVAK,
+    ]
+]
+
+squadsk_tasks = [
+    LightevalTaskConfig(
+        name=f"community_quad_{language.value}",
+        prompt_function=get_qa_prompt_function(
+            language,
+            lambda line: {
+                "question": line["question"],
+                "context": line["context"],
+                "choices": [ans for ans in line["answers"]["text"] if len(ans) > 0],
+            },
+        ),
+        suite=("community",),
+        hf_repo="TUKE-DeutscheTelekom/squad-sk",
+        hf_subset="plain_text",
+        evaluation_splits=("validation",),
+        hf_filter=lambda line: any(len(ans) > 0 for ans in line["answers"]["text"]),
+        few_shots_split="train",
+        generation_size=400,
+        stop_sequence=("\n",),
+        metric=(
+            multilingual_quasi_exact_match_metric(language, "prefix"),
+            multilingual_quasi_f1_score_metric(language),
+        ),
+    )
+    for language in [
+        Language.SLOVAK,
+    ]
+]
+
+mqa_tasks_slovak = [
+    LightevalTaskConfig(
+        name=f"community_mqa_{language.value}",
+        prompt_function=get_qa_prompt_function(
+            language,
+            lambda line: {
+                "question": line["name"],
+                "choices": [line["answers"][0]["text"]],
+            },
+        ),
+        suite=("community",),
+        hf_repo="clips/mqa",
+        hf_subset="sk-all-question",
+        evaluation_splits=("train",),
+        few_shots_select=None,
+        generation_size=400,
+        stop_sequence=("\n",),
+        metric=(
+            multilingual_quasi_exact_match_metric(language, "prefix"),
+            multilingual_quasi_f1_score_metric(language),
+        ),
+    )
+    for language in [
+        Language.SLOVAK,
+    ]
+]
+
+webfaq_tasks_slovak = [
+    LightevalTaskConfig(
+        name=f"community_webfaq_{language.value}",
+        prompt_function=get_qa_prompt_function(
+            language,
+            lambda line: {
+                "question": line["question"],
+                "choices": [line["answer"]],
+            },
+        ),
+        suite=("community",),
+        hf_repo="PaDaS-Lab/webfaq",
+        hf_subset="slk",
+        evaluation_splits=("default",),
+        few_shots_select=None,
+        generation_size=400,
+        stop_sequence=("\n",),
+        metric=(
+            multilingual_quasi_exact_match_metric(language, "prefix"),
+            multilingual_quasi_f1_score_metric(language),
+        ),
+    )
+    for language in [
+        Language.SLOVAK,
+    ]
+]
+
+qa2dsk_tasks = [
+    LightevalTaskConfig(
+        name=f"community_qa2d_{language.value}",
+        prompt_function=get_qa_prompt_function(
+            language,
+            lambda line: {
+                "question": line["question"],
+                "choices": [line["answer"], line['turker_answer']],
+            },
+        ),
+        suite=("community",),
+        hf_repo="ctu-aic/qa2d-sk",
+        hf_subset="default",
+        evaluation_splits=("validation",),
+        few_shots_split="train",
+        generation_size=400,
+        stop_sequence=("\n",),
+        metric=(
+            multilingual_quasi_exact_match_metric(language, "prefix"),
+            multilingual_quasi_f1_score_metric(language),
+        ),
+    )
+    for language in [
+        Language.SLOVAK,
+    ]
+]
+
+def mathbio(line, task_name: str = None):
+    query = f"The following are multiple choice questions (with answers) about {line['category_en']}.\n\n"
+    query += line["question"] + "\n"
+    query += "".join([f"{key}. {choice}\n" for key, choice in zip(LETTER_INDICES, line["options"])])
+    query += "Answer:"
+
+    gold_ix = int(line["answer"]) - 1
+    is_few_shots = line.get("__few_shots", False)  # We are adding few shots
+
+    return Doc(
+        task_name=task_name,
+        query=query,
+        choices=[" A", " B", " C", " D"] if is_few_shots else ["A", "B", "C", "D"],
+        gold_index=gold_ix,
+        instruction=f"The following are multiple choice questions (with answers) about {line['category_en']}.\n\n",
+    )
+
+mathbio_slovak = LightevalTaskConfig(
+    name="community_mathbio_sk",
+    suite=("community",),
+    prompt_function=mathbio,
+    hf_repo="dokato/exam-slovak-mathbio",
+    hf_subset="default",
+    hf_avail_splits=["train",],
+    evaluation_splits=["train"],
+    few_shots_select=None,
+    generation_size=1,
+    metric=[Metrics.loglikelihood_acc],
+    stop_sequence=["\n"],
+    trust_dataset=True,
+    version=0,
+)
+
+def mathbio_sk(line, task_name: str = None):
+    query = f"Nasledujú otázky s výberom odpovedí (s možnosťou výberu z viacerých odpovedí) týkajúce sa Matematiky.\n\n"
+    query += line["question"] + "\n"
+    query += "".join([f"{key}. {choice}\n" for key, choice in zip(LETTER_INDICES, line["options"])])
+    query += "Odpoveď:"
+
+    gold_ix = int(line["answer"]) - 1
+    is_few_shots = line.get("__few_shots", False)  # We are adding few shots
+
+    return Doc(
+        task_name=task_name,
+        query=query,
+        choices=[" A", " B", " C", " D"] if is_few_shots else ["A", "B", "C", "D"],
+        gold_index=gold_ix,
+        instruction=f"Nasledujú otázky s výberom odpovedí (s možnosťou výberu z viacerých odpovedí) týkajúce sa Matematiky.\n\n",
+    )
+
+
+mathbio_slovak_prompt = LightevalTaskConfig(
+    name="community_mathbio_sk_prompt",
+    suite=("community",),
+    prompt_function=mathbio_sk,
+    hf_repo="dokato/exam-slovak-mathbio",
+    hf_subset="default",
+    hf_avail_splits=["train",],
+    evaluation_splits=["train"],
+    few_shots_select=None,
+    generation_size=1,
+    metric=[Metrics.loglikelihood_acc],
+    stop_sequence=["\n"],
+    trust_dataset=True,
+    version=0,
+)
+
+def boolq_sk(line, task_name: str = None):
+    # remove extra `?`
+    # check if question ends with `?` if not add it
+    question = line["question"].strip()
+    if not question.endswith("?"):
+        question += "?"
+    return Doc(
+        task_name=task_name,
+        query=f"Passage: {line['passage']}\nQuestion: {question}\nAnswer:",
+        choices=[" Yes", " No"],
+        gold_index=[True, False].index(line["answer"]),
+    )
+    
+def boolq_sk_prompt(line, task_name: str = None):
+    # remove extra `?`
+    # check if question ends with `?` if not add it
+    question = line["question"].strip()
+    if not question.endswith("?"):
+        question += "?"
+    return Doc(
+        task_name=task_name,
+        query=f"Úryvok: {line['passage']}\nOtázka: {question}\nOdpoveď:",
+        choices=[" Áno", " Nie"],
+        gold_index=[True, False].index(line["answer"]),
+    )
+
+boolq_slovak = LightevalTaskConfig(
+    name="community_boolq_sk",
+    suite=("community",),
+    prompt_function=boolq_sk,
+    hf_repo="crabz/boolq_sk",
+    hf_subset="default",
+    hf_avail_splits=["train", "validation"],
+    evaluation_splits=["validation"],
+    few_shots_select=None,
+    generation_size=5,
+    metric=[
+        Metrics.exact_match,
+        Metrics.quasi_exact_match,
+        Metrics.prefix_exact_match,
+        Metrics.prefix_quasi_exact_match,
+    ],
+    stop_sequence=["\n"],
+    trust_dataset=True,
+    version=0,
+)
+
+boolq_slovak_prompt = LightevalTaskConfig(
+    name="community_boolq_sk_prompt",
+    suite=("community",),
+    prompt_function=boolq_sk_prompt,
+    hf_repo="crabz/boolq_sk",
+    hf_subset="default",
+    hf_avail_splits=["train", "validation"],
+    evaluation_splits=["validation"],
+    few_shots_select=None,
+    generation_size=5,
+    metric=[
+        Metrics.exact_match,
+        Metrics.quasi_exact_match,
+        Metrics.prefix_exact_match,
+        Metrics.prefix_quasi_exact_match,
+    ],
+    stop_sequence=["\n"],
+    trust_dataset=True,
+    version=0,
+)
+
+def senti(line, task_name: str = None):
+    return Doc(
+        task_name=task_name,
+        query=f"Úryvok: {line['text']}\nSentiment: ",
+        choices=["Pozitívne", "Negatívne", "Neutrálne"],
+        gold_index=["positive", "negative", "neutral"].index(line["label"]),
+    )
+
+senti_sk = LightevalTaskConfig(
+    name="community_senti_sk_prompt",
+    suite=("community",),
+    prompt_function=senti,
+    hf_repo="TUKE-KEMT/senti-sk",
+    hf_subset="default",
+    hf_avail_splits=["train", "test"],
+    evaluation_splits=["test"],
+    few_shots_select=None,
+    generation_size=5,
+    metric=[
+        Metrics.exact_match,
+        Metrics.quasi_exact_match,
+        Metrics.prefix_exact_match,
+        Metrics.prefix_quasi_exact_match,
+        Metrics.f1_score_macro,
+        Metrics.f1_score_micro,
+    ],
+    stop_sequence=["\n"],
+    trust_dataset=True,
+    version=0,
+)
+
+def slovak_sa(line, task_name: str = None):
+    return Doc(
+        task_name=task_name,
+        query=f"Úryvok: {line['text']}\nSentiment: ",
+        choices=["Negatívne", "Pozitívne"],
+        gold_index=line["label"],
+    )
+
+slovak_sa_task = LightevalTaskConfig(
+    name="community_slovak_sa_prompt",
+    suite=("community",),
+    prompt_function=slovak_sa,
+    hf_repo="DGurgurov/slovak_sa",
+    hf_subset="default",
+    hf_avail_splits=["train", "validation", "test"],
+    evaluation_splits=["test"],
+    few_shots_split="train",
+    few_shots_select="random_sampling_from_train",
+    generation_size=5,
+    metric=[
+        Metrics.exact_match,
+        Metrics.quasi_exact_match,
+        Metrics.prefix_exact_match,
+        Metrics.prefix_quasi_exact_match,
+        Metrics.f1_score_macro,
+        Metrics.f1_score_micro,
+    ],
+    stop_sequence=["\n"],
+    trust_dataset=True,
+    version=0,
+)
+
+def toxic_sk(line, task_name: str = None):
+    query = f"Je nasledujúci text toxický?\nText: {line['text']}\nOdpoveď:"
+    
+    return Doc(
+        task_name=task_name,
+        query=query,
+        choices=["Nie", "Áno"],
+        gold_index=line["label"],
+    )
+
+toxic_sk_task = LightevalTaskConfig(
+    name="community_toxic_sk_prompt",
+    suite=("community",),
+    prompt_function=toxic_sk,
+    hf_repo="TUKE-KEMT/toxic-sk",
+    hf_subset="default",
+    hf_avail_splits=["train", "test"],
+    evaluation_splits=["test"],
+    few_shots_split="train",
+    few_shots_select="random_sampling_from_train",
+    generation_size=5,
+    metric=[
+        Metrics.exact_match,
+        Metrics.quasi_exact_match,
+        Metrics.prefix_exact_match,
+        Metrics.prefix_quasi_exact_match,
+        Metrics.f1_score_macro,
+        Metrics.f1_score_micro,
+    ],
+    stop_sequence=["\n"],
+    trust_dataset=True,
+    version=0,
+)
+
+def hate_speech(line, task_name: str = None):
+    query = f"Predstavuje nasledujúci text hate speech?\nText: {line['text']}\nOdpoveď:"
+    
+    return Doc(
+        task_name=task_name,
+        query=query,
+        choices=["Nie", "Áno"],
+        gold_index=line["label"],
+    )
+
+hatespeech_sk_task = LightevalTaskConfig(
+    name="community_hatespeech_sk_prompt",
+    suite=("community",),
+    prompt_function=hate_speech,
+    hf_repo="TUKE-KEMT/hate_speech_slovak",
+    hf_subset="default",
+    hf_avail_splits=["train", "test"],
+    evaluation_splits=["test"],
+    few_shots_split="train",
+    few_shots_select="random_sampling_from_train",
+    generation_size=5,
+    metric=[
+        Metrics.exact_match,
+        Metrics.quasi_exact_match,
+        Metrics.prefix_exact_match,
+        Metrics.prefix_quasi_exact_match,
+        Metrics.f1_score_macro,
+        Metrics.f1_score_micro,
+    ],
+    stop_sequence=["\n"],
+    trust_dataset=True,
+    version=0,
+)
+
+def get_sib_function(
+    language: Language,
+    formulation: Formulation = MCFFormulation(),
+):
+    def prompt_fn(line, task_name: str = None):
+        choices = [
+            "geography",
+            "science/technology",
+            "entertainment",
+            "politics",
+            "health",
+            "travel",
+            "sports",
+        ]
+        
+        slovak_choices = [
+            "geografia",
+            "veda/technológie",
+            "zábava",
+            "politika",
+            "zdravie",
+            "cestovanie",
+            "šport",
+        ]
+
+        translation_literals = TRANSLATION_LITERALS[language]
+
+        instruction_val = "Aká je kategória nasledujúceho textu?"
+        instruction = f"{instruction_val}\n" if instruction_val else ""
+        
+        context = ""
+
+        question = capitalize(fix_ending_punct(line["text"], translation_literals))
+        answers = [capitalize(fix_ending_punct(str(answer), translation_literals)) for answer in slovak_choices]
+
+        options = build_choices(answers, formulation, translation_literals)
+        options = f"{options}\n" if options else ""
+        answers = build_answers(answers, formulation, translation_literals)
+
+        answer_word = capitalize(translation_literals.answer)
+        question_word = capitalize(translation_literals.question_word)
+
+        query = MULTI_CHOICE_QA_QUERY.format(
+            instruction=instruction,
+            question=question,
+            context=context,
+            question_word=question_word,
+            answer_word=answer_word,
+            colon=translation_literals.colon,
+            sentence_space=translation_literals.sentence_space,
+            options=options,
+        )
+        
+        # each text has text category as label
+        gold_idx = choices.index(line["category"]) if line["category"] in choices else None
+
+        return Doc(
+            task_name=task_name,
+            query=query,
+            gold_index=as_list(gold_idx),
+            choices=answers,
+            instruction=instruction_val,
+            unconditioned_query=f"{answer_word}{translation_literals.colon}",
+        )
+        
+    return prompt_fn
+    
+sib200_sk_task = [
+    LightevalTaskConfig(
+        name=f"community_sib200_sk_prompt_{formulation.name.lower()}",
+        suite=("community",),
+        prompt_function=get_sib_function(
+            Language.SLOVAK,
+            formulation=formulation,
+        ),
+        hf_repo="mteb/sib200",
+        hf_subset="slk_Latn",
+        hf_avail_splits=["train", "test"],
+        evaluation_splits=["test"],
+        few_shots_split="train",
+        few_shots_select="random_sampling_from_train",
+        metric=get_metrics_for_formulation(
+            formulation,
+            [
+                loglikelihood_acc_metric(normalization=LogProbTokenNorm()),
+                loglikelihood_acc_metric(normalization=LogProbCharNorm()),
+                loglikelihood_acc_metric(normalization=LogProbPMINorm()),
+            ],
+        ),
+        trust_dataset=True,
+        version=0,
+    )
+    
+    for formulation in [
+        MCFFormulation(),
+        CFFormulation(),
+        HybridFormulation(),
+    ]
+]
+
 
 TASKS_TABLE = [
     *mlmm_hellaswag_tasks,
